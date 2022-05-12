dc26bfa945e53d75eb46e9682cfced9e76ac8e1a
Lutfi Aldri Permana <12050110415@students.uin-suska.ac.id>
2022-05-12 23:01:32 +0700

    XML

---
+++ app/src/main/java/com/example/uts/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registerlayout);
+
+        Button btnCreate = findViewById(R.id.btnCreate);
+        EditText etUsername = findViewById(R.id.etUsername);
+        EditText etFullname = findViewById(R.id.etFullname);
+        EditText etPassword = findViewById(R.id.etPassword);
+
+
+
+    }
+
+
+    public void Login(View view) {
+    }
+}
